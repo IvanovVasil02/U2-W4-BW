6436c00ddd7e91472132ca952b5b592a20ce8056
mariangelamarasciuolo <130611642+mariangelamarasciuolo@users.noreply.github.com>
2023-08-31 14:55:53 +0200

    Add code for album

---
+++ assets/Javascript/script.js
@@ -0,0 +1,36 @@
+/* const URL = "https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem";
+const getData = async function () {
+  const resp = await fetch(URL);
+  const responseConvert = await resp.json();
+   console.log(resp);
+    console.log(responseConvert.data);
+  responseConvert.data.forEach((songObj, index) => {
+    const container = document.getElementById("cont-song");
+    container.innerHTML += `<div class="row">
+      <div class="col-auto d-flex justify-content-end align-items-center">
+        <p>${index + 1}</p>
+      </div>
+      <div class="col-1 d-flex justify-content-end align-items-center px-2">
+        <img src=${songObj.album.cover_medium} class="img-fluid" alt="" />
+      </div>
+      <div class="col d-flex align-items-center ps-1">
+        <h6>${songObj.title}</h6>
+      </div>
+      
+      <div class="col d-flex justify-content-end align-items-center">
+        <p>${songObj.rank}</p>
+      </div>
+      <div class="col d-flex justify-content-end align-items-center pe-4">
+        <p>${songObj.duration}</p>
+      </div>
+    </div>`;
+
+    console.log(songObj);
+  });
+ 
+};
+
+window.onload = function () {
+  getData();
+};
+ */
