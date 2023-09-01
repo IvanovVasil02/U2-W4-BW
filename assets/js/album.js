578cae438add9f36db5fd01e5d5a31382708fb76
mariangelamarasciuolo <130611642+mariangelamarasciuolo@users.noreply.github.com>
2023-09-01 14:26:31 +0200

    Sidebar Scroll

--- assets/js/album.js
+++ assets/js/album.js
@@ -24,6 +24,23 @@ const loadData = async () => {
     const albums = await response.json();
 
     loadMainCard(albumCard, albums.title, albums.cover_medium, albums.artist.name);
+    albums.tracks.data.forEach((track, index) => {
+      albumListSongs.innerHTML += `<div class="row">
+    <div class="col d-flex align-items-center pt-2">
+      <span class="pe-3 ps-1">${index + 1}</span>
+      <div class="title-song d-flex flex-column justify-content-center">
+        <h6>${track.title}</h6>
+        <p>${albums.artist.name}</p>
+      </div>
+    </div>
+    <div class="col d-flex justify-content-end align-items-center">
+      <p>${track.rank}</p>
+    </div>
+    <div class="col d-flex justify-content-end align-items-center pe-4">
+      <p>${(track.duration / 60).toFixed(2)}</p>
+    </div>
+  </div>`;
+    });
 
     console.log(albums);
   } catch (error) {
